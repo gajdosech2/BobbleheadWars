17e7bb61e377bdbe5d25523ff8374932f8b59f22
gajdosech2 <l.gajdosech@gmail.com>
2020-02-14 13:08:06 +0100

    chapter 6 animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
 	public Rigidbody head;
 	public LayerMask layerMask;
 	private Vector3 currentLookTarget = Vector3.zero;
+	public Animator bodyAnimator;
 
 	// Use this for initialization
 	void Start () {
@@ -31,11 +32,13 @@ public class PlayerController : MonoBehaviour {
 		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		if (moveDirection == Vector3.zero)
         {
-			//TODO
-        } else
+			bodyAnimator.SetBool("IsMoving", false);
+        }
+		else
         {
 			head.AddForce(transform.right * 150, ForceMode.Acceleration);
-        }
+			bodyAnimator.SetBool("IsMoving", true);
+		}
 
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
